b54ad89acd09a7cad27cefcb7b1b8fb298e2cf97
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-04-25 00:14:23 +0200

    Was neues
    Change-Id: I16e8e9414c3ce5c0c473a6fdde1cdcbe544b5962

--- src/de/unistuttgart/dsa2013/ws1/SpeedList.java
+++ src/de/unistuttgart/dsa2013/ws1/SpeedList.java
@@ -7,7 +7,7 @@ package de.unistuttgart.dsa2013.ws1;
  * @author Viso
  *
  */
-public class SpeedList<T> {
+public class SpeedList<T> implements A1aCollection<T> {
 	public T value;
 	public SpeedList<T> next = null;
 	public SpeedList<T> achtnext = null;
@@ -19,6 +19,27 @@ public class SpeedList<T> {
 	public SpeedList(T value, SpeedList<T> nextNode) {
 		this.value = value;
 		this.next = nextNode;
+	}
+	
+	public void prepend (T item) {
+		SpeedList<T> newFirst = new SpeedList<T> (item, firstNode);
+		firstNode = newFirst;
+	}
+	
+	public int size() {
+		int i = 0;
+		SpeedList<T> zeiger = firstNode;
+		
+		if (zeiger == null)
+			return i;
+		
+		do {
+			i++;
+			zeiger = zeiger.next;			
+		} while (zeiger.next != null);
+		
+		return i;
+		
 	}
 }
 
