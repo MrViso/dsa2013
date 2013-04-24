b8666af85dae20dde938eb65f2218f21d484cb1d
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-04-25 00:24:48 +0200

    Version 1 sollte fertig sein
    Change-Id: Id767788f1fd40941efe29044c930ccf44773ab26

--- src/de/unistuttgart/dsa2013/ws1/SpeedList.java
+++ src/de/unistuttgart/dsa2013/ws1/SpeedList.java
@@ -8,27 +8,17 @@ package de.unistuttgart.dsa2013.ws1;
  *
  */
 public class SpeedList<T> implements A1aCollection<T> {
-	public T value;
-	public SpeedList<T> next = null;
-	public SpeedList<T> achtnext = null;
-	
-	/**
-	 *  Constructor fuer die SpeedList.
-	 *
-	 */
-	public SpeedList(T value, SpeedList<T> nextNode) {
-		this.value = value;
-		this.next = nextNode;
-	}
+
+	SpeedNode<T> firstNode;
 	
 	public void prepend (T item) {
-		SpeedList<T> newFirst = new SpeedList<T> (item, firstNode);
+		SpeedNode<T> newFirst = new SpeedNode<T> (item, firstNode);
 		firstNode = newFirst;
 	}
 	
 	public int size() {
 		int i = 0;
-		SpeedList<T> zeiger = firstNode;
+		SpeedNode<T> zeiger = firstNode;
 		
 		if (zeiger == null)
 			return i;
