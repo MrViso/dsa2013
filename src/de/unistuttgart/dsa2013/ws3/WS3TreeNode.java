414aadbe2bbe4e7ff8c56e19bbdaa9798d561b6b
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-05-07 00:07:03 +0200

    Neues Package für WS3
    Change-Id: I09598046a6387bfcea878e58e9c7541349b81f76

---
+++ src/de/unistuttgart/dsa2013/ws3/WS3TreeNode.java
@@ -0,0 +1,157 @@
+package de.unistuttgart.dsa2013.ws3;
+
+/**
+ * ToDo
+ *
+ * @param <E> The type of the values stored in the nodes of the tree.
+ */
+public class WS3TreeNode<E> {
+	
+	/**
+	 * Initializes a new instance.
+	 * 
+	 * @param n The maximum number of children.
+	 * @throws IllegalArgumentException if n is less than one.
+	 */
+	@SuppressWarnings("unchecked")
+	public WS3TreeNode(int n) {
+		if (n < 1) {
+			throw new IllegalArgumentException("ToDo");
+		}
+		
+		children = (WS3TreeNode<E>[])new WS3TreeNode[n];
+	}
+	
+	/**
+	 * Initializes a new instance with a parent node.
+	 * 
+	 * @param parent The parent node.
+	 * @param n The maximum number of children.
+	 * @throws IllegalArgumentException if parent is null, or if n is less than one.
+	 */
+	private WS3TreeNode(WS3TreeNode<E> parent, int n) {
+		this(n);
+		if (parent == null) {
+			throw new IllegalArgumentException("The parent must not be null.");
+		}
+		
+		this.parent = parent;
+	}
+	
+	/**
+	 * The parent node.
+	 */
+	private WS3TreeNode<E> parent;
+	
+	/**
+	 * Retrieves the parent node.
+	 * 
+	 * @return the parent node, or null if this is a root node.
+	 */
+	public WS3TreeNode<E> getParent() {
+		return parent;
+	}
+	
+	/**
+	 * Indicates whether the current node is a root node of a tree.
+	 * 
+	 * @return a value that indicates whether this instance is a root node.
+	 */
+	public boolean isRoot() {
+		return parent == null;
+	}
+	
+	/**
+	 * The array of child nodes.
+	 */
+	private WS3TreeNode<E>[] children;
+	
+	/**
+	 * Adds a child node.
+	 * 
+	 * @param value the new child node.
+	 * @return The newly added child node.
+	 */
+	public WS3TreeNode<E> addChild(E value) {
+		if (childCount < children.length) {
+			WS3TreeNode<E> newNode = new WS3TreeNode<E>(this, children.length);
+			newNode.setValue(value);
+			children[childCount] = newNode;
+			childCount++;
+			return newNode;
+		} else {
+			throw new IllegalStateException
+					("The maximum number of children has already been reached.");
+		}
+	}
+	
+	/**
+	 * The number of child nodes.
+	 */
+	private int childCount = 0;
+	
+	/**
+	 * Returns the number of child nodes.
+	 * 
+	 * @return the number of child nodes.
+	 */
+	public int getChildCount() {
+		return childCount;
+	}
+	
+	/**
+	 * Removes a child node at a given position.
+	 * 
+	 * @param index the position of the child node to remove.
+	 * @throws IndexOutOfBoundsException if index is outside of the valid range.
+	 */
+	public void removeChildAt(int index) {
+		if ((index < 0) || (index >= childCount)) {
+			throw new IndexOutOfBoundsException("The index is outside of the valid range.");
+		}
+		
+		childCount--;
+		for (int i = index; i < childCount; i++) {
+			children[i] = children[i + 1];
+		}
+		children[childCount] = null;
+	}
+	
+	/**
+	 * Retrieves a child node at a given position.
+	 * 
+	 * @param index the position of the child.
+	 * @return the child node at the indicated position.
+	 * @throws IndexOutOfBoundsException if index is outside of the valid range.
+	 */
+	public WS3TreeNode<E> getChild(int index) {
+		if ((index < 0) || (index >= childCount)) {
+			throw new IndexOutOfBoundsException("The index is outside of the valid range.");
+		}
+		
+		return children[index];
+	}
+	
+	/**
+	 * The value of the tree node.
+	 */
+	private E value;
+	
+	/**
+	 * Gets the value stored in the tree node.
+	 * 
+	 * @return the value of the tree node.
+	 */
+	public E getValue() {
+		return value;
+	}
+	
+	/**
+	 * Sets the value stored in the tree node.
+	 * 
+	 * @param value the new value of the tree node.
+	 */
+	public void setValue(E value) {
+		this.value = value;
+	}
+}
