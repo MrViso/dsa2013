462e113caf76d0b8306d0ff84e3748c1a6abb0d2
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-05-16 00:19:09 +0200

    Aktuellste Version
    Change-Id: I593073fe18b71d0e86b091c132cf1788cfabac94

--- src/de/unistuttgart/dsa2013/ws4/DisplayTree2c.java
+++ src/de/unistuttgart/dsa2013/ws4/DisplayTree2c.java
@@ -1,5 +1,30 @@
+/**
+ * 
+ */
 package de.unistuttgart.dsa2013.ws4;
 
+/**
+ * Eine einfache Implementation des grafischen Baums
+ * 
+ * @author Maximilian Visotschnig
+ * @author Frank Merkle
+ * @author Alessandro Tridico
+ * @history 2013-05-15 MV 1.0 
+ * @version 2013-05-15 MV 1.0 Erste Version
+ * 
+ */
 public class DisplayTree2c {
+
+	/**
+	 * Ausfuehrende Methode
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GenericTreeNode<Integer> tree = MyTreeFactory.createTree();
+		MyTreeInfoProvider<Integer> infoProvider = new MyTreeInfoProvider<Integer>();
+		TreeDisplay.showTree(tree, infoProvider);
+
+	}
 
 }
--- src/de/unistuttgart/dsa2013/ws4/MyTreeInfoProvider.java
+++ src/de/unistuttgart/dsa2013/ws4/MyTreeInfoProvider.java
@@ -1,5 +1,52 @@
 package de.unistuttgart.dsa2013.ws4;
 
-public class MyTreeInfoProvider {
+/**
+ * Implementation fuer optimale Ausgabe der Knoten.
+ * 
+ * @author Maximilian Visotschnig
+ * @author Frank Merkle
+ * @author Alessandro Tridico
+ * @history 2013-05-15 MV 1.0 
+ * @version 2013-05-15 MV 1.0 Erste Version
+ * 
+ * @param <T>
+ *            Der Datentyp der in den Knoten gespeichert wird.
+ */
+public class MyTreeInfoProvider<T> implements TreeInfoProvider<T> {
+
+	/**
+	 * Gibt den horizontalen Abstand zurueck.
+	 * 
+	 * @return Horizontalabstand
+	 */
+	@Override
+	public int getHSpacing() {
+		return 250;
+	}
+
+	/**
+	 * Gibt den vertikalen Abstand zurueck.
+	 * 
+	 * @return Vertikalabstand
+	 */
+	@Override
+	public int getVSpacing() {
+		return 50;
+	}
+
+	/**
+	 * Berechnet den Abstand zwischen Knoten und Kindsknoten
+	 * 
+	 * @param sizeStorage
+	 *            Berechnete Groessen die gespeichert werden.
+	 * @param root
+	 *            Wurzel des Baums
+	 * @throws IllegalArgumentException
+	 *             Falls eines der Argumente null ist
+	 */
+	@Override
+	public void computeSizes(TreeSizeStorage<T> sizeStorage,
+			GenericTreeNode<T> root) {
+	}
 
 }
