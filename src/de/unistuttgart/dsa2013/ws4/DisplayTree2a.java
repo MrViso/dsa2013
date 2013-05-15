7fecbbb3c49309fb4776d9a27f9327a24d02bb58
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-05-15 23:47:49 +0200

    Erste Version
    Change-Id: If9db9861bea832dbabdacf4316122804b8389a9c

--- src/de/unistuttgart/dsa2013/ws4/DisplayTree2a.java
+++ src/de/unistuttgart/dsa2013/ws4/DisplayTree2a.java
@@ -4,16 +4,26 @@
 package de.unistuttgart.dsa2013.ws4;
 
 /**
- * @author Viso
- *
+ * Eine einfache Implementation des grafischen Baums
+ * 
+ * @author Maximilian Visotschnig
+ * @author Frank Merkle
+ * @author Alessandro Tridico
+ * @history 2013-05-15 MV 1.0 
+ * @version 2013-05-15 MV 1.0 Erste Version
+ * 
  */
 public class DisplayTree2a {
 
 	/**
+	 * Ausfuehrende Methode
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GenericTreeNode<Integer> tree = TreeFactory.createSmallTree();
+		DummyTreeInfoProvider<Integer> infoProvider = new DummyTreeInfoProvider<Integer>();
+		TreeDisplay.showTree(tree, infoProvider);
 
 	}
 
