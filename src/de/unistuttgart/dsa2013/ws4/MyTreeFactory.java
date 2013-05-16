7f5574d7a4a04dfe3924e6844c74cb796a22f40c
Aless <Aless@Aless-PC.fritz.box>
2013-05-16 16:12:41 +0200

    a

--- src/de/unistuttgart/dsa2013/ws4/MyTreeFactory.java
+++ src/de/unistuttgart/dsa2013/ws4/MyTreeFactory.java
@@ -6,18 +6,18 @@ package de.unistuttgart.dsa2013.ws4;
  * @author Maximilian Visotschnig
  * @author Frank Merkle
  * @author Alessandro Tridico
- * @version 2013-05-15 MV 1.0 
- * @history 2013-05-15 MV 1.0 Erste Version 
+ * @history 2013-05-15 MV 1.0 
+ * @version 2013-05-15 MV 1.0 Erste Version
  * 
  */
 public final class MyTreeFactory {
-
+	
 	/**
 	 * Hidden constructor.
 	 */
 	private MyTreeFactory() {
 	}
-
+	
 	/**
 	 * Erschafft den Baum mit elf Knoten
 	 * 
@@ -25,24 +25,23 @@ public final class MyTreeFactory {
 	 */
 	public static GenericTreeNode<Integer> createTree() {
 		GenericTreeNode<Integer> root = new WS4TreeNode<Integer>(2, 19);
-
+		
 		GenericTreeNode<Integer> c1 = root.addChild(12);
 		GenericTreeNode<Integer> c2 = root.addChild(85);
-
-		c1.addChild(11);
+		
+		c1.addChild(10);
 		c1.addChild(16);
+		
 
-		c1.getChildAt(0).addChild(9);
-
-		c1.getChildAt(1).addChild(14);
-
+		c1.getChildAt(0).addChild(11);
+	
+		
 		c2.addChild(40);
 		c2.addChild(95);
-
 		
 		c2.getChildAt(0).addChild(26);
 		c2.getChildAt(0).addChild(44);
-
+		
 		return root;
 	}
 }
