fe87a3062424c2d8b399197c254804dd31596088
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-05-23 01:55:47 +0200

    Change-Id: Ib403bd5de92d1cf54b2ecffd633e236c3d20f5d8

--- src/de/unistuttgart/dsa2013/bws1/main.java
+++ src/de/unistuttgart/dsa2013/bws1/main.java
@@ -3,6 +3,8 @@ package de.unistuttgart.dsa2013.bws1;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 public class main {
 	
 	void forSchleife (){
@@ -13,17 +15,21 @@ public class main {
 		}
 	}
 	
-	void whileSchleifeA (){
-		ArrayList <String> items = new ArrayList <String>();
+
+	static void whileSchleifeA (ArrayList <String> eingang){
+		ArrayList <String> items = eingang;
+		//ArrayList <String> items = new ArrayList <String>();
 		int i = 0;
 		while (i < items.size()){
-			processValue(items.get(i));		
+			processValue(items.get(i));	
+			i++;
 		}
 		
 	}
 	
-	void whileSchleifeB (){
-		ArrayList <String> items = new ArrayList <String>();
+	static void whileSchleifeB (ArrayList <String> eingang){
+		ArrayList <String> items = eingang;
+		//ArrayList <String> items = new ArrayList <String>();
 		Iterator<String> itemIterator = items.iterator();
 		while (itemIterator.hasNext()){
 			processValue(itemIterator.next());		
@@ -33,4 +39,19 @@ public class main {
 	
 	
 
+	private static void processValue(String item) {
+		// TODO Auto-generated method stub
+		System.out.println(item);
+	}
+	
+	public static void main(String[] args){
+		ArrayList <String> eingang = new ArrayList <String>();
+		eingang.add("1");
+		eingang.add("2");
+		eingang.add("3");
+		eingang.add("4");
+		eingang.add("5");
+		whileSchleifeA(eingang);
+		whileSchleifeB(eingang);
+	}
 }
