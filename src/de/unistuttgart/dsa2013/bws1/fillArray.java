df0b5ea7ee7b1ead84e9c84917598dcb907c5c3c
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-05-23 04:11:47 +0200

    Nen bissl rübergearbeitet...aber kA ob das so überhaupt gedacht ist ;)
    Change-Id: Icced4a085dc9b4bd06eb342197ded45a98a481fb

--- src/de/unistuttgart/dsa2013/bws1/fillArray.java
+++ src/de/unistuttgart/dsa2013/bws1/fillArray.java
@@ -1,38 +1,47 @@
 package de.unistuttgart.dsa2013.bws1;
 
-private static void fillArray (Integer[][] array, int value){
-	if (array == null) {
-		throw new IllegalArgumentException("array must not be null.");
-	}
-	
-	int column = 0;
-	int line = 0;
-	int value = 0;
-	
-	system.out.println("Welchen Wert einfügen?");
-	value = system.in.line;							//muss man nicht-int eingaben abfangen oder geht des automatisch?
-	
-	system.out.println("In welche Zeile einfügen?");
-	line = system.in.line;							
-	if (line > array.length) {
-		throw new IllegalArgumentException("array has only " + array.length + " lines");
-	}
-	
-	system.out.println("In welche Spalte einfügen?");
-	column = system.in.line;
-	if (column > array[line].length) {
-		throw new IllegalArgumentException("array has only " + array[line].length + " columns!");
-	}
-	
-	array[line][column] = value;
-}
-}
+import java.io.IOException;
+import java.util.Arrays;
 
-private static void fillArrayInc ( Integer [][] array ){
+public class fillArray {
 	
-	for (i=0, i < array.length, i++) {
-		for (j=0, j < array[i].length, j++) {
-			array[i][j] = i + j;
+	private static void fillArray(Integer[][] array, int value)
+			throws IllegalArgumentException, IOException {
+		if (array == null) {
+			throw new IllegalArgumentException("array must not be null.");
+		}
+
+		int column = 0;
+		int line = 0;
+		int value1 = 0;
+
+		System.out.println("Welchen Wert einfügen?");
+		value1 = System.in.read(); // muss man nicht-int eingaben abfangen oder
+									// geht des automatisch?
+
+		System.out.println("In welche Zeile einfügen?");
+		line = System.in.read();
+		if (line > array.length) {
+			throw new IllegalArgumentException("array has only " + array.length
+					+ " lines");
+		}
+
+		System.out.println("In welche Spalte einfügen?");
+		column = System.in.read();
+		if (column > array[line].length) {
+			throw new IllegalArgumentException("array has only "
+					+ array[line].length + " columns!");
+		}
+
+		array[line][column] = value1;
+	}
+
+	private static void fillArrayInc ( Integer [][] array ){
+		
+		for (int i=0, i < array.size() , i++) {
+			for (int j=0, j < array[i].size(), j++) {
+				array[i][j] = i + j;
+			}
 		}
 	}
 }
