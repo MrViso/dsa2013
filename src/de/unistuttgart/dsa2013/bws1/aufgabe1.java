cae0d7c81bc958dbabeb6b91ae1bd2f90f16a470
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-05-23 02:30:15 +0200

    Sollte passen
    Change-Id: I039c6e2136971749680948ad7dea9f4d6f8c36b4

--- src/de/unistuttgart/dsa2013/bws1/aufgabe1.java
+++ src/de/unistuttgart/dsa2013/bws1/aufgabe1.java
@@ -3,15 +3,30 @@ package de.unistuttgart.dsa2013.bws1;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-
+/**
+ * Implementierung von verschiedenen Schleifen
+ * 
+ * @author Maximilian Visotschnig
+ * @author Frank Merkle
+ * @author Alessandro Tridico
+ * @version 2013-05-23 MV 1.0
+ * @history 2013-05-23 MV 1.0 Implementierung der Schleifen
+ * 
+ */
 public class aufgabe1 {
 
 	/**
+	 * Urspruengliche Vorgabe in einer Methode umgesetzt.
+	 * 
+	 * ArrayList <String> items = new ArrayList <String>();
+	 * wurde zum Besseren Testen zum Kommentar gemacht und in die main gezogen.
+	 * 
 	 * 
 	 * @param eingang
 	 */
 	static void forSchleife(ArrayList<String> eingang) {
 		ArrayList<String> items = eingang;
+		// ArrayList <String> items = new ArrayList <String>();
 
 		for (String item : items) {
 			processValue(item);
@@ -19,8 +34,14 @@ public class aufgabe1 {
 	}
 
 	/**
+	 * Aufgabe 1a: Implementierung einer While Schleife mit Index i, welcher alle
+	 * Elemente des Arrays durchgeht und weitergibt.
+	 * 
+	 * ArrayList <String> items = new ArrayList <String>();
+	 * wurde zum Besseren Testen zum Kommentar gemacht und in die main gezogen.
 	 * 
 	 * @param eingang
+	 *            einlesen eines String Arrays
 	 */
 	static void whileSchleifeA(ArrayList<String> eingang) {
 		ArrayList<String> items = eingang;
@@ -34,8 +55,15 @@ public class aufgabe1 {
 	}
 
 	/**
+	 * Aufgabe 1b: Implementierung einer whileSchleife, welche mit Hilfe eines
+	 * Iterators alle ArrayList Elemente durchgeht und an processValue
+	 * uebergibt. 
+	 * 
+	 * ArrayList <String> items = new ArrayList <String>();
+	 * wurde zum Besseren Testen zum Kommentar gemacht und in die main gezogen.
 	 * 
 	 * @param eingang
+	 *            einlesen eines String Arrays
 	 */
 	static void whileSchleifeB(ArrayList<String> eingang) {
 		ArrayList<String> items = eingang;
@@ -46,40 +74,76 @@ public class aufgabe1 {
 		}
 
 	}
-	
+
 	/**
+	 * Aufgabe 1c: Implementierung einer For-Schleife welche mit Hilfe eines
+	 * Iterators den naechsten Wert ausliest und daraufhin in eine Variable
+	 * speichert 
+	 * 
+	 * ArrayList <String> items = new ArrayList <String>(); wurde zum
+	 * Besseren Testen zum Kommentar gemacht und in die main gezogen.
 	 * 
 	 * @param eingang
+	 *            einlesen eines String Arrays
 	 */
 	static void forSchleifeC(ArrayList<String> eingang) {
 		ArrayList<String> items = eingang;
 		// ArrayList <String> items = new ArrayList <String>();
 		Iterator<String> itemIterator = items.iterator();
-		for (int i = 0; i < items.size(); i++){
+		for (int i = 0; i < items.size(); i++) {
 			String item = itemIterator.next();
 			processValue(item);
 		}
-		
+
 	}
 
+	/**
+	 * Eine einfache implementation der Methode, welches erlaubt die Elemente
+	 * direkt auszugeben.
+	 * 
+	 * @param item
+	 *            Auszugebendes Element.
+	 */
 	private static void processValue(String item) {
 		System.out.println(item);
 	}
 
+	
+	/**
+	 * Main Methode, welche alle Methoden der Klasse testet.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		ArrayList<String> eingang = new ArrayList<String>();
-		eingang.add("1");
-		eingang.add("2");
-		eingang.add("3");
-		eingang.add("4");
-		eingang.add("5");
+
+		// Hier findet sich die erste Zeile der Quelltextvorgabe!!
+		ArrayList<String> items = new ArrayList<String>();
+
+		// Array mit Werten vorsorgen
+		items.add("1");
+		items.add("2");
+		items.add("3");
+		items.add("4");
+		items.add("5");
+
+		// Test der vorgegebenen for-Schleife
 		System.out.println("Ausgangs for-schleife");
-		forSchleife(eingang);
+		forSchleife(items);
+		System.out.println("--");
+
+		// Test der While Schleife mit Index
 		System.out.println("Aufgabe 1a: While Schleife mit Index");
-		whileSchleifeA(eingang);
+		whileSchleifeA(items);
+		System.out.println("--");
+
+		// Test der While Schleife mit Iterator
 		System.out.println("Aufgabe 1b: While Schleife mit Iterator");
-		whileSchleifeB(eingang);
+		whileSchleifeB(items);
+		System.out.println("--");
+
+		// Test der For Schleife mit Iterator
 		System.out.println("Aufgabe 1c: For Schleife mit Iterator");
-		forSchleifeC(eingang);
+		forSchleifeC(items);
+		System.out.println("--");
 	}
 }
