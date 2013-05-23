75746e2b539878a2cce2eb9e9a8ce1867d8d72f2
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-05-23 02:11:25 +0200

    Strange
    Change-Id: I7f1df2b880af609c795726612c30ef49142def61

--- src/de/unistuttgart/dsa2013/bws1/main.java
+++ src/de/unistuttgart/dsa2013/bws1/main.java
@@ -3,55 +3,84 @@ package de.unistuttgart.dsa2013.bws1;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 public class main {
-	
-	void forSchleife (){
-		ArrayList <String> items = new ArrayList <String>();
-		
-		for (String item : items){
+
+	/**
+	 * 
+	 * @param eingang
+	 */
+	static void forSchleife(ArrayList<String> eingang) {
+		ArrayList<String> items = eingang;
+
+		for (String item : items) {
 			processValue(item);
 		}
 	}
-	
 
-	static void whileSchleifeA (ArrayList <String> eingang){
-		ArrayList <String> items = eingang;
-		//ArrayList <String> items = new ArrayList <String>();
+	/**
+	 * 
+	 * @param eingang
+	 */
+	static void whileSchleifeA(ArrayList<String> eingang) {
+		ArrayList<String> items = eingang;
+		// ArrayList <String> items = new ArrayList <String>();
 		int i = 0;
-		while (i < items.size()){
-			processValue(items.get(i));	
+		while (i < items.size()) {
+			processValue(items.get(i));
 			i++;
 		}
-		
+
+	}
+
+	/**
+	 * 
+	 * @param eingang
+	 */
+	static void whileSchleifeB(ArrayList<String> eingang) {
+		ArrayList<String> items = eingang;
+		// ArrayList <String> items = new ArrayList <String>();
+		Iterator<String> itemIterator = items.iterator();
+		while (itemIterator.hasNext()) {
+			processValue(itemIterator.next());
+		}
+
 	}
 	
-	static void whileSchleifeB (ArrayList <String> eingang){
-		ArrayList <String> items = eingang;
-		//ArrayList <String> items = new ArrayList <String>();
+	/**
+	 * 
+	 * @param eingang
+	 */
+	static void forSchleifeC(ArrayList<String> eingang) {
+		ArrayList<String> items = eingang;
+		// ArrayList <String> items = new ArrayList <String>();
 		Iterator<String> itemIterator = items.iterator();
-		while (itemIterator.hasNext()){
-			processValue(itemIterator.next());		
+		for (int i = 0; i < items.size(); i++){
+			String item = itemIterator.next();
+			processValue(item);
 		}
 		
 	}
-	
-	
 
 	private static void processValue(String item) {
 		// TODO Auto-generated method stub
 		System.out.println(item);
 	}
-	
-	public static void main(String[] args){
-		ArrayList <String> eingang = new ArrayList <String>();
+
+	public static void main(String[] args) {
+		ArrayList<String> eingang = new ArrayList<String>();
 		eingang.add("1");
 		eingang.add("2");
 		eingang.add("3");
 		eingang.add("4");
 		eingang.add("5");
+		System.out.println("Ausgangs for-schleife");
+		forSchleife(eingang);
+		System.out.println("Aufgabe 1a: While Schleife mit Index");
 		whileSchleifeA(eingang);
+		System.out.println("Aufgabe 1b: While Schleife mit Iterator");
 		whileSchleifeB(eingang);
+		System.out.println("Aufgabe 1c: For Schleife mit Iterator");
+		forSchleifeC(eingang);
 	}
 }
