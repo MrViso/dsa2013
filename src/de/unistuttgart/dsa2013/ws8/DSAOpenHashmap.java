3a46c37b25604e739f2fba910dbb4cfe2cdca5bd
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-06-20 01:33:07 +0200

    blablabla
    Change-Id: Ic75b4f9a815fbb3c008e393b4f7f220582c146cd

--- src/de/unistuttgart/dsa2013/ws8/DSAOpenHashmap.java
+++ src/de/unistuttgart/dsa2013/ws8/DSAOpenHashmap.java
@@ -2,31 +2,42 @@ package de.unistuttgart.dsa2013.ws8;
 
 import java.util.Iterator;
 
-public class DSAOpenHashmap <K, V> implements DSAMapD<K, V> {
+public class DSAOpenHashmap<K, V> implements DSAMapD<K, V> {
+	private final KeyValuePair<K, V>[] table;
+	private final int c;
 
-
-	
-	
 	public DSAOpenHashmap(int size, int c){
-		this.table = KeyValuePair<K,V>[size];
+		if (c<1)
+			throw new IllegalArgumentException("c muss mindestens 1 groß sein");
+		this.table = new KeyValuePair[size];
 		this.c = c;
 		
 	}
-	
-	private final KeyValuePair<K,V>[] table;
 
-	private final int c;
-	
-	
+
+
 	@Override
 	public Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>> iterator() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>>(table);
 	}
 
 	@Override
 	public V put(K key, V value) {
-		// TODO Auto-generated method stub
+		int index = getIndex(key);
+		if (table[index] == null){
+			table[index] = new KeyValuePair<K, V>(key, value);
+		}
+		
+		for (KeyValuePair<K, V> entry : table[index]){
+			if (entry.getKey().equals(key)){
+				V oldValue  = entry.getValue();
+				entry.setValue(value);
+				return oldValue;
+			}
+		}
+		table[index].addLast(new KeyValuePair<K,V>(key, value));		
+		
 		return null;
 	}
 
@@ -38,13 +49,32 @@ public class DSAOpenHashmap <K, V> implements DSAMapD<K, V> {
 
 	@Override
 	public V get(K key) {
-		// TODO Auto-generated method stub
+		int index = getIndex(key);
+		if (table[index] != null){
+			for (KeyValuePair<K, V> entry : table[index]){
+				if (entry.getKey().equals(key)){
+					return entry.getValue();
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public V remove(K key) {
-		// TODO Auto-generated method stub
+		int index = getIndex(key);
+		if (table[index] != null){
+			Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>> it = table[index].iterator();
+			
+			while (it.hasNext()){
+				KeyValuePair<K,V> entry = it.next();
+				if (entry.getKey().equals(key)){
+					it.remove();
+					return entry.getValue();
+				}
+			}
+		}
+		
 		return null;
 	}
 
@@ -55,6 +85,5 @@ public class DSAOpenHashmap <K, V> implements DSAMapD<K, V> {
 	public int getC() {
 		return c;
 	}
-	
-	
+
 }
