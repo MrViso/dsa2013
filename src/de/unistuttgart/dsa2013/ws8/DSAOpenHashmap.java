f28adb09c745332a37060f47d334ca80de6339c7
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-06-20 16:49:01 +0200

    So ein scheiß
    Change-Id: I5c28a4729986744af5fdfd0942ebc2c8eb716818

--- src/de/unistuttgart/dsa2013/ws8/DSAOpenHashmap.java
+++ src/de/unistuttgart/dsa2013/ws8/DSAOpenHashmap.java
@@ -2,57 +2,151 @@ package de.unistuttgart.dsa2013.ws8;
 
 import java.util.Iterator;
 
+/**
+ * Implementation des NodeExpanders
+ * 
+ * @author Maximilian Visotschnig
+ * @author Frank Merkle
+ * @author Alessandro Tridico
+ * @version 2013-06-20 MV 1.0
+ * @history 2013-06-20 MV 1.0 Erste Version
+ * 
+ * @param <K>
+ *            Schlüssel Typ
+ * @param <V>
+ *            Werte Typ
+ * 
+ */
 public class DSAOpenHashmap<K, V> implements DSAMapD<K, V> {
 	private final KeyValuePair<K, V>[] table;
 	private final int c;
 
-	public DSAOpenHashmap(int size, int c){
-		if (c<1)
+	/**
+	 * Initialisiert eine Hashmap
+	 * 
+	 * @param size
+	 *            Anzahl der Elemente
+	 * @param c
+	 *            Schrittweite
+	 * @throws IllegalArgumentException
+	 *             if c<1
+	 */
+	public DSAOpenHashmap(int size, int c) throws IllegalArgumentException{
+		if (c < 1)
 			throw new IllegalArgumentException("c muss mindestens 1 groß sein");
-		this.table = new KeyValuePair[size];
+		table = new KeyValuePair[size];
 		this.c = c;
-		
+
 	}
 
-
-
+	/**
+	 * Iterator
+	 * 
+	 * @return Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>>
+	 *         Den Iterator der genutzt werden soll
+	 * 
+	 */
 	@Override
 	public Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>> iterator() {
 		// TODO Auto-generated method stub
-		return new Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>>(table);
+		return new Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>>(
+				table);
 	}
 
+	/**
+	 * Methode put fuegt einen Wert in die Hashmap hinzu, wenn dieser noch nicht
+	 * enthalten ist.
+	 * 
+	 * @param key
+	 *            Der Schlüssel des Eintrags
+	 * @param value
+	 *            der Wert des Eintrags
+	 * @throws IllegalArgumentException
+	 *             if key or value is null
+	 */
 	@Override
-	public V put(K key, V value) {
+	public V put(K key, V value) throws IllegalArgumentException {
+		if (key == null)
+			throw new IllegalArgumentException("Key ist null in V put");
+		if (value == null)
+			throw new IllegalArgumentException("Value ist null in V put");
+
 		int index = getIndex(key);
-		if (table[index] == null){
+		if (table[index] == null) {
 			table[index] = new KeyValuePair<K, V>(key, value);
 		}
-		
-		for (KeyValuePair<K, V> entry : table[index]){
-			if (entry.getKey().equals(key)){
-				V oldValue  = entry.getValue();
+
+		for (KeyValuePair<K, V> entry : this) {
+			if (entry.getKey().equals(key)) {
+				V oldValue = entry.getValue();
 				entry.setValue(value);
 				return oldValue;
 			}
 		}
-		table[index].addLast(new KeyValuePair<K,V>(key, value));		
-		
+		table[index].addLast(new KeyValuePair<K, V>(key, value));
+
 		return null;
 	}
 
+	/**
+	 * Index des Keys heraussuchen
+	 * 
+	 * @param key
+	 *            der in Index gewandelt werden muss
+	 * @return int gesuchter Index für die Hashmap
+	 * 
+	 * @throw IllegalArgumentException wenn key null ist
+	 */
+	private int getIndex(K key) throws IllegalArgumentException {
+		if (key == null)
+			throw new IllegalArgumentException("key ist bei int getIndex null");
+		return Math.abs(key.hashCode()) % table.length;
+	}
+
+	/**
+	 * Prüft ob Key vorhanden ist
+	 * 
+	 * @param key
+	 *            nach dem gesucht werden soll
+	 * @return Wahrheitswert ob Key vorhanden oder nicht
+	 * @throws IllegalArgumentException
+	 *             falls Key null ist
+	 */
 	@Override
-	public boolean containsKey(K key) {
-		// TODO Auto-generated method stub
+	public boolean containsKey(K key) throws IllegalArgumentException {
+		if (key == null)
+			throw new IllegalArgumentException(
+					"Schlüssel ist bei boolean containsKey null");
+
+		Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>> it = this
+				.iterator();
+		while (it.hasNext()) {
+			KeyValuePair<K, V> entry = it.next();
+			if (entry.getKey() == key) {
+				return true;
+			}
+		}
 		return false;
 	}
 
+	/**
+	 * Gibt einen Wert aus, der nach seinem Key gesucht wird
+	 * 
+	 * @param key
+	 *            der Schlüssel des gesuchten Werts
+	 * @return der gesuchte Wert
+	 * @throws IllegalArgumentException
+	 *             wenn key null ist
+	 * 
+	 */
 	@Override
-	public V get(K key) {
+	public V get(K key) throws IllegalArgumentException {
+		if (key == null)
+			throw new IllegalArgumentException("Schlüssel ist bei V get null");
 		int index = getIndex(key);
-		if (table[index] != null){
-			for (KeyValuePair<K, V> entry : table[index]){
-				if (entry.getKey().equals(key)){
+		if (table[index] != null) {
+			for (KeyValuePair<K, V> entry : this) {
+				if (entry.getKey().equals(key)) {
 					return entry.getValue();
 				}
 			}
@@ -60,28 +154,52 @@ public class DSAOpenHashmap<K, V> implements DSAMapD<K, V> {
 		return null;
 	}
 
+	/**
+	 * Entfernt einen Eintrag ausgehend von seinem Schlüssel
+	 * 
+	 * @param key
+	 *            Der Schlüssel der auf den zu entfernenden Wert zeigt
+	 * @return Den Wert der entfernt werden soll
+	 * @throws IllegalArgumentException
+	 *             Wenn key null ist
+	 * 
+	 */
 	@Override
-	public V remove(K key) {
+	public V remove(K key) throws IllegalArgumentException {
+		if (key == null)
+			throw new IllegalArgumentException(
+					"Schlüssel ist bei V remove null");
 		int index = getIndex(key);
-		if (table[index] != null){
-			Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>> it = table[index].iterator();
-			
-			while (it.hasNext()){
-				KeyValuePair<K,V> entry = it.next();
-				if (entry.getKey().equals(key)){
+		if (table[index] != null) {
+			Iterator<de.unistuttgart.dsa2013.ws8.DSAMapA.KeyValuePair<K, V>> it = this
+					.iterator();
+
+			while (it.hasNext()) {
+				KeyValuePair<K, V> entry = it.next();
+				if (entry.getKey().equals(key)) {
 					it.remove();
 					return entry.getValue();
 				}
 			}
 		}
-		
+
 		return null;
 	}
 
+	/**
+	 * Gibt den Table zurück
+	 * 
+	 * @return Der auszugebende Table
+	 */
 	public KeyValuePair<K, V>[] getTable() {
 		return table;
 	}
 
+	/**
+	 * Gibt die Schrittweite c aus
+	 * 
+	 * @return Schrittweite c
+	 */
 	public int getC() {
 		return c;
 	}
