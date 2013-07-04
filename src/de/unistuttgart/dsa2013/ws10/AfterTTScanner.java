daf4575c7f89fa019322e152f0f3dd77d043de9a
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-07-04 17:02:35 +0200

    Change-Id: Ib5355ca122395c370c08c5425417ac850472fff0

--- src/de/unistuttgart/dsa2013/ws10/AfterTTScanner.java
+++ src/de/unistuttgart/dsa2013/ws10/AfterTTScanner.java
@@ -1,5 +1,10 @@
 package de.unistuttgart.dsa2013.ws10;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Implementierung des LineScanner Interfaces mit dem Hintergrund, dass Woerter
  * darauf geprueft werden, ob sie mit einem oder mehreren 't's Enden. Ggf. werden
@@ -14,10 +19,34 @@ package de.unistuttgart.dsa2013.ws10;
  */
 public class AfterTTScanner implements LineScanner {
 
+	//TODO
+	private static final Pattern p = Pattern.compile("\\w+");
+	
+	
+	/**
+	 * Iterates over line and matches with the pattern, if the last letters are
+	 * one or more 't's. If yes, the word will be added
+	 * 
+	 * 
+	 * @param line String which should be checked
+	 * @throws IllegalArgumentException
+	 *             if line is null
+	 */
 	@Override
-	public Iterable<String> retrieveStrings(String completeLine) {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterable<String> retrieveStrings(String line) {
+		if (line == null){
+			throw new IllegalArgumentException("line is null");
+		}
+		
+		
+		//TODO
+		List<String> result = new ArrayList<String>();
+		
+		Matcher m = p.matcher(line);
+		while (m.find()) {
+			result.add(m.group());
+		}
+		
+		return result;
 	}
-
 }
--- src/de/unistuttgart/dsa2013/ws10/CapitalWordScanner.java
+++ src/de/unistuttgart/dsa2013/ws10/CapitalWordScanner.java
@@ -1,5 +1,10 @@
 package de.unistuttgart.dsa2013.ws10;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Implementierung des LineScanner Interfaces mit dem Hintergrund, dass Woerter
  * darauf geprueft werden ob sie mit einem Grossbuchstaben starten- Solche Worte
@@ -14,10 +19,30 @@ package de.unistuttgart.dsa2013.ws10;
  */
 public class CapitalWordScanner implements LineScanner {
 
-	@Override
-	public Iterable<String> retrieveStrings(String completeLine) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private static final Pattern p = Pattern.compile("\\p{Upper}");
 
+	/**
+	 * Iterates over line and matches with the pattern, if the first letter is
+	 * capital size. If yes, the word will be added
+	 * 
+	 * 
+	 * @param line String which should be checked
+	 * @throws IllegalArgumentException
+	 *             if line is null
+	 */
+	@Override
+	public Iterable<String> retrieveStrings(String line) {
+		if (line == null) {
+			throw new IllegalArgumentException("line is null");
+		}
+
+		List<String> result = new ArrayList<String>();
+
+		Matcher m = p.matcher(line);
+		while (m.find()) {
+			result.add(m.group());
+		}
+
+		return result;
+	}
 }
