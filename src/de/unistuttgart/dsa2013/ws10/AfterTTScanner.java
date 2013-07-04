a8b760f58fe98f175f2783b07d2e775e386902b5
Maximilian Visotschnig <maximilian.visotschnig@gmx.net>
2013-07-04 17:09:18 +0200

    Change-Id: Ib1ee5a289e9bc466072127503c7683b1891cca3d

--- src/de/unistuttgart/dsa2013/ws10/AfterTTScanner.java
+++ src/de/unistuttgart/dsa2013/ws10/AfterTTScanner.java
@@ -20,7 +20,8 @@ import java.util.regex.Pattern;
 public class AfterTTScanner implements LineScanner {
 
 	//TODO
-	private static final Pattern p = Pattern.compile("\\w+");
+	private static final Pattern p = Pattern.compile("\\t");
+	private static final Pattern p2 = Pattern.compile("\\tt*");
 	
 	
 	/**
